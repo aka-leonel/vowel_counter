9d4c2b208ecb25706d814b29c1a18d8443b6b5c4
Leonel Godoy <60699907+aka-leonel@users.noreply.github.com>
2022-03-27 14:04:17 -0300

    funciona. plotea en un mismo grafico el conteo de voales en espaniol e ingles. falta hacer que pueda leer los textos deste archivos

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # vowel_counter
 Simple python code for counting vowel 
+
+
+------------------
+
+#El objetivo de este ejercicio es notar cuales son las vocales mas usadas
+#  tanto en español como en inglés y comparar
+
+#Ambos textos son el comienzo de Alicia en el pais de las maravillas, de Lewis Carroll.
+
+Aquí se practican muchas cosas:
+*uso de matplotlib para graficos
+*arrays
+*diccionarios, extraccion de claves/valores
+*crear e importar un modulo propio (biblioteca de funciones)
+*se trabaja con Github, claro
+*funciones con *args
+
+Contador de letras. Primera version-solo cuenta vocales.
---
+++ myFun.py
@@ -0,0 +1,34 @@
+# Funcion contadora--------------------------
+def countVocals(text):
+    myDic = {"a": 0,
+             "e": 0,
+             "i": 0,
+             "o": 0,
+             "u": 0}
+    #to_lowerCase
+    text = text.lower()
+
+    # Python does not have (switch) statement
+    for x in text:
+        if x == "a":
+            myDic["a"] = myDic["a"]+1
+        if x == "e":
+            myDic["e"] = myDic["e"]+1
+        if x == "i":
+            myDic["i"] = myDic["i"]+1
+        if x == "o":
+            myDic["o"] = myDic["o"]+1
+        if x == "u":
+            myDic["u"] = myDic["u"]+1
+        
+    # print("a: ", myDic["a"])
+    # print("e: ", myDic["e"])
+    # print("i: ", myDic["i"])
+    # print("o: ", myDic["o"])
+    # print("u: ", myDic["u"])
+    
+    totalVocales = myDic["a"] + myDic["e"] + myDic["i"] + myDic["o"] + myDic["u"]
+    print("longitud texto: ", len(text))
+    print("total vocales: ", totalVocales)
+    print("no-vocales: ", len(text)-totalVocales)
+    return myDic
