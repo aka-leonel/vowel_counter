63bb5a560f0fa6c8613f3a8b4b4ab6329a3a8d59
Leonel Godoy <60699907+aka-leonel@users.noreply.github.com>
2022-03-27 15:04:51 -0300

    ch3t0 m4l, mi pequeño programa ya cuenta todas las letras del alfabeto. Debo decir que la solucion fue poco elegante (declarar un diccionario de 27 elementos)

--- myFun.py
+++ myFun.py
@@ -4,7 +4,30 @@ def countVocals(text):
              "e": 0,
              "i": 0,
              "o": 0,
-             "u": 0}
+             "u": 0,
+             "b":0,
+             "c":0,
+             "d":0,
+             "f":0,
+             "g":0,
+             "h":0,
+             "j":0,
+             "k":0,
+             "l":0,
+             "m":0,
+             "n":0,
+             "ñ":0,
+             "p":0,
+             "q":0,
+             "r":0,
+             "s":0,
+             "t":0,
+             "v":0,
+             "w":0,
+             "x":0,
+             "y":0,
+             "z":0             
+             }
     #to_lowerCase
     text = text.lower()
 
@@ -20,6 +43,50 @@ def countVocals(text):
             myDic["o"] = myDic["o"]+1
         if x == "u":
             myDic["u"] = myDic["u"]+1
+        if x == "b":
+            myDic["b"] = myDic["b"]+1
+        if x == "c":
+            myDic["c"] = myDic["c"]+1
+        if x == "d":
+            myDic["d"] = myDic["d"]+1
+        if x == "f":
+            myDic["f"] = myDic["f"]+1
+        if x == "g":
+            myDic["g"] = myDic["g"]+1
+        if x == "h":
+            myDic["h"] = myDic["h"]+1
+        if x == "j":
+            myDic["j"] = myDic["j"]+1
+        if x == "k":
+            myDic["k"] = myDic["k"]+1
+        if x == "l":
+            myDic["l"] = myDic["l"]+1
+        if x == "m":
+            myDic["m"] = myDic["m"]+1
+        if x == "n":
+            myDic["n"] = myDic["n"]+1
+        if x == "ñ":
+            myDic["ñ"] = myDic["ñ"]+1
+        if x == "p":
+            myDic["p"] = myDic["p"]+1
+        if x == "q":
+            myDic["q"] = myDic["q"]+1
+        if x == "r":
+            myDic["r"] = myDic["r"]+1
+        if x == "s":
+            myDic["s"] = myDic["s"]+1
+        if x == "t":
+            myDic["t"] = myDic["t"]+1
+        if x == "v":
+            myDic["v"] = myDic["v"]+1
+        if x == "w":
+            myDic["w"] = myDic["w"]+1
+        if x == "x":
+            myDic["x"] = myDic["x"]+1
+        if x == "y":
+            myDic["y"] = myDic["y"]+1
+        if x == "z":
+            myDic["z"] = myDic["z"]+1
         
     print("a: ", myDic["a"])
     print("e: ", myDic["e"])
